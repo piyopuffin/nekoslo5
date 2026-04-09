924a7e177978cfcf60eef0a60e5d022851d40178
PiyoPuffin <>
2026-04-09 17:35:57 +0900

    workflow: ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/nekoslo5/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
